0774299c33073b3cef329af0c4edf0efa76b55d7
BoeJ <jboe@nevada.unr.edu>
2022-12-07 15:57:34 -0800

    Monopoly

---
+++ Monopoly.py
@@ -0,0 +1,34 @@
+import rospy
+import math
+import sys
+import os
+
+
+class Monopoly():
+	def __init__(self):
+		self.board = Board()
+		self.turn = 'player'
+		self.gameover = False
+		
+	def changeTurn(self):
+		if self.turn == 'player':
+			self.turn == 'robot'
+		else:
+			self.turn == 'player'
+			
+	def gameOver	
+		
+
+
+
+
+class Board():
+	def__init__(self):
+	
+	
+	
+	
+	
+	
+if __name__=="__main__":
+    main()
