ba557b92a8d73c9eee9653d5e63536abf0b32f4b
yhtham108 <yhtham108@nevada.unr.edu>
2022-12-07 16:39:09 -0800

    Update ik_pick_and_place_demo.py
    Auto add in board, piece, and dice.

--- ik_pick_and_place_demo.py
+++ ik_pick_and_place_demo.py
@@ -143,12 +143,12 @@ class PickAndPlace(object):
         # retrieve current pose from endpoint
         current_pose = self._limb.endpoint_pose()
         ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x
-        ik_pose.position.y = current_pose['position'].y
+        ik_pose.position.x = current_pose['position'].x 
+        ik_pose.position.y = current_pose['position'].y 
         ik_pose.position.z = current_pose['position'].z + self._hover_distance
-        ik_pose.orientation.x = current_pose['orientation'].x
-        ik_pose.orientation.y = current_pose['orientation'].y
-        ik_pose.orientation.z = current_pose['orientation'].z
+        ik_pose.orientation.x = current_pose['orientation'].x 
+        ik_pose.orientation.y = current_pose['orientation'].y 
+        ik_pose.orientation.z = current_pose['orientation'].z 
         ik_pose.orientation.w = current_pose['orientation'].w
         joint_angles = self.ik_request(ik_pose)
         # servo up from current pose
@@ -183,18 +183,30 @@ class PickAndPlace(object):
 
 def load_gazebo_models(table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
                        table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.6725, y=0.1265, z=0.7825)),
-                       block_reference_frame="world"):
+                       tophat_pose=Pose(position=Point(x=1.375, y=-0.35, z=0.7845)),
+                       tophat_reference_frame="world", 
+                       monopoly_pose=Pose(position=Point(x=1.0, y=0.0, z=0.7825)),
+                       monopoly_reference_frame="world",
+                       redDie_pose=Pose(position=Point(x=1.0, y=0.0, z=0.7845)),
+                       redDie_reference_frame="world") :
     # Get Models' Path
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
     # Load Table SDF
     table_xml = ''
     with open (model_path + "cafe_table/model.sdf", "r") as table_file:
         table_xml=table_file.read().replace('\n', '')
-    # Load Block URDF
-    block_xml = ''
-    with open (model_path + "block/model.urdf", "r") as block_file:
-        block_xml=block_file.read().replace('\n', '')
+    # Load Board SDF
+    monopoly_xml = ''
+    with open (model_path + "monopoly/model.sdf", "r") as monopoly_file:
+        monopoly_xml=monopoly_file.read().replace('\n', '')
+    # Load Die SDF
+    die_xml = ''
+    with open (model_path + "redDie/model.sdf", "r") as redDie_file:
+        redDie_xml=redDie_file.read().replace('\n', '')        
+    # Load Block SDF
+    tophat_xml = ''
+    with open (model_path + "tophat/model.sdf", "r") as tophat_file:
+        tophat_xml=tophat_file.read().replace('\n', '')
     # Spawn Table SDF
     rospy.wait_for_service('/gazebo/spawn_sdf_model')
     try:
@@ -203,12 +215,30 @@ def load_gazebo_models(table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
                              table_pose, table_reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-    # Spawn Block URDF
+        
+    # Spawn Monopoly Board SDF    
+    rospy.wait_for_service('/gazebo/spawn_sdf_model')
+    try:
+        spawn_sdf_2 = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+        resp_sdf_2 = spawn_sdf("monopoly", monopoly_xml, "/",
+                             monopoly_pose, monopoly_reference_frame)
+    except rospy.ServiceException, e:
+        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
+    # Spawn Die SDF
+    rospy.wait_for_service('/gazebo/spawn_sdf_model')
+    try:
+        spawn_sdf_3 = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+        resp_sdf_3 = spawn_sdf("redDie", redDie_xml, "/",
+                             redDie_pose, redDie_reference_frame)
+    except rospy.ServiceException, e:
+        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
+    
+    # Spawn tophat SDF
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
     try:
-        spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block", block_xml, "/",
-                               block_pose, block_reference_frame)
+        spawn_sdf_4 = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+        resp_sdf_4 = spawn_sdf("tophat", tophat_xml, "/",
+                               tophat_pose, tophat_reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn URDF service call failed: {0}".format(e))
 
@@ -220,18 +250,18 @@ def delete_gazebo_models():
     try:
         delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
         resp_delete = delete_model("cafe_table")
-        resp_delete = delete_model("block")
+        resp_delete = delete_model("tophat")
+        resp_delete = delete_model("monopoly")
+        resp_delete = delete_model("redDie")
     except rospy.ServiceException, e:
         rospy.loginfo("Delete Model service call failed: {0}".format(e))
 
 def main():
     """RSDK Inverse Kinematics Pick and Place Example
-
     A Pick and Place example using the Rethink Inverse Kinematics
     Service which returns the joint angles a requested Cartesian Pose.
     This ROS Service client is used to request both pick and place
     poses in the /base frame of the robot.
-
     Note: This is a highly scripted and tuned demo. The object location
     is "known" and movement is done completely open loop. It is expected
     behavior that Baxter will eventually mis-pick or drop the block. You
@@ -282,11 +312,7 @@ def main():
     pnp.move_to_start(starting_joint_angles)
     idx = 0
     while not rospy.is_shutdown():
-        print("\nPicking...")
-        pnp.pick(block_poses[idx])
-        print("\nPlacing...")
-        idx = (idx+1) % len(block_poses)
-        pnp.place(block_poses[idx])
+        print("Hello World!")
     return 0
 
 if __name__ == '__main__':
