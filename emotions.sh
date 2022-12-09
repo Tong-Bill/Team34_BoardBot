ac084d03b5b65c460f5d4386c5e18071651ba95f
Bill Tong <66698504+Tong-Bill@users.noreply.github.com>
2022-12-09 02:23:27 -0800

    Baxter Emoticons added

---
+++ emotions.sh
@@ -0,0 +1,32 @@
+source /home/bill/baxterws/devel/setup.bash
+
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/image1.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/thinking1.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/excited.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/thinking2.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/happy.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/sad.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/neutral.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/ecstatic.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/unhappy.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/surprised.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/wink.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/confused.png;
+sleep 3
+rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/photos/dead.png;
+
+
+
+
+
