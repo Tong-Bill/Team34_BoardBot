f59ee5614a81908378d8b5b75ad81f83f5cb8c13
yhtham108 <yhtham108@nevada.unr.edu>
2023-03-02 12:56:30 -0800

    Update AI.py
    Added more functionalities. Now just more of a blueprint, but need input from those working on Computer Vision and Rules.

--- AI.py
+++ AI.py
@@ -7,7 +7,7 @@
 #from std_msgs.msg import String
 from collections import defaultdict
 
-board = defaultdict(list)
+board = {}
 board["Boardwalk"] = [False, 0, "Blue", 270] #Owned, Owned by who, color set, price
 #Just an example of the dictionary of properties. 
 
@@ -16,8 +16,6 @@ class MoneyExchange:
     self.robotMoney = {500:2, 100:2, 50:2, 20:6, 10:5, 5:5, 1:5}
     self.robotMoneySum = 1500
     #Gives command to grab or count the correct amount of starting money. 
-    self.opponentMoney = {500:2, 100:2, 50:2, 20:6, 10:5, 5:5, 1:5}
-    self.opponentMoneySum = 1500
   def gainMoney(self, amount):
     self.robotMoneySum += amount
     while amount > 0:
@@ -46,7 +44,7 @@ class MoneyExchange:
           
 class Properties(MoneyExchange):
   def __init__(self):
-    self.ownedProperty = defaultdict(list)
+    self.ownedProperty = {}
     self.ownedProperty["Blue"] = [0]
   def checkPropertyOwnership(self, list): #Computer vision will pass in square 
     if list[0] == False:
@@ -54,6 +52,7 @@ class Properties(MoneyExchange):
     return False
   def PurchaseProperty(self, square):
     if self.checkPropertyOwnership(board[square]):
+      print("Hello World")
       #Implement intellegent way to determine whether to buy the property
       #if going to buy:
         #self.loseMoney(board[square][3]) Lose the amount in the price
@@ -63,15 +62,77 @@ class Properties(MoneyExchange):
         #self.ownedProperty[board[square][2]].append(square)
         #self.ownedProperty[board[square][2]][0] += 1
         #update the board and owned properties function
+    
   #def setsComplete(self):
   #def buildHouseHotel(self):
 
-#class Jail:
+class Jail:
+  def __init__(self):
+    self.card = False
+    self.inJail = False
+    self.bail = 50
+    self.otherJail = False
+    self.visiting = False
 
-#class Auction: (Potential, not necessary)
-#class TurnLog:
-#class opponentHandler: (Includes getting and paying rent, as well as keeping track of the opponents properties)
+  def getOutofJail(self, card): #card here is a bool of whether the robot has the get out of jail free card.
+    if self.inJail and self.card:
+      self.card = False
+      self.inJail = False
+      #TakeTurn Function is called
+    else:
+      #Intelligent way of determining whether to pay the fine or roll. Rolling means you don't get another turn. 
+      print("Hello World") #This is just standing in for other code. I am building the blueprint of the code.
     
+#class Auction: (Potential, not necessary)
+class TurnLog: #Creates a log of their turn in a txt file. (Potential but not necessary)
+  def __init__(self):
+    f = open("monopolyLog.txt", "w")
+    
+class opponentHandler(Properties): #(Includes getting and paying rent, as well as keeping track of the opponents properties)
+  def __init__(self):
+    self.opponentMoney = {500:2, 100:2, 50:2, 20:6, 10:5, 5:5, 1:5}
+    self.opponentMoneySum = 1500
+    self.opponentProperties = {}
+
+  #def giveMoney(self):
+
+  #def getMoney(self):
+
+  #def cards(self):
+    
+  #def buyProperties(self):
+
+  #def sellProperties(self):
+
+  #def completeSets(self):
+
+  #def landInJail(self):
+
+
+class GameOver(Properties):
+  def __init__(self):
+    self.turnCount = 0
+    self.turnLimit = 50
+    self.bankrupt = False
+
+  def outOfMoney(self):
+    for i in self.robotMoney:
+      if self.robotMoney[i] != 0:
+        return False
+    for i in self.ownedProperty:
+      if self.ownedProperty[i][0] != 0:
+        return False
+    return True
+
+  def endGame(self):
+    if self.outOfMoney():
+      print("Looks like I'm out of money. You win! Would you like to play again?")
+      #Sadness in social aspects
+      exit(-1)
+    #Find a way for the robot to know that the opponent lost. 
+    elif self.turnCount == self.turnLimit:
+      #Compare the net worths of both players and determines who wins
+
 #def main():
   #call roll dice
   
