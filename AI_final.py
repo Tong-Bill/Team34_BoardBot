5f7e3e2f7eb1adc82157e52954ade41c835ca88b
BoeJ <58668323+BoeJ@users.noreply.github.com>
2023-04-21 19:34:09 -0700

    Update AI_final.py

--- AI_final.py
+++ AI_final.py
@@ -4,45 +4,45 @@ import random as rd
 #new Board which is a dict that is [type of square, color/name, name, weight]. Weight is used for selling properties and is found using data online on which properties are the most valuable. 
 class board(rules.BoardSpaces, rules.TitleDeedCards):
   Board = { 0: [-2, "Go"], 
-            1: [0, "Brown", "Mediterrean Avenue", 0.2], 
+            1: [0, "Brown", "Mediterrean Avenue"], 
             2: [-2, "CommunityChest"],
-            3: [0, "Brown", "Baltic Avenue", 0.2],
+            3: [0, "Brown", "Baltic Avenue"],
             4: [-2, "IncomeTax"],
-            5: [0, "Railroad", "Reading Railroad", 0.8], 
-            6: [0, "LightBlue", "Oriental Avenue", 0.3],
+            5: [0, "Railroad", "Reading Railroad"], 
+            6: [0, "LightBlue", "Oriental Avenue"],
             7: [-2, "Chance"],
-            8: [0, "LightBlue", "Vermont Avenue", 0.3], 
-            9: [0, "LightBlue", "Connecticut Avenue", 0.3], 
+            8: [0, "LightBlue", "Vermont Avenue"], 
+            9: [0, "LightBlue", "Connecticut Avenue"], 
             10:[-2, "Jail"],
-            11:[0, "Magenta", "St Charles Place", 0.4],
-            12:[0, "Utilities", "Electric Company", 0.1],
-            13:[0, "Magenta", "States Avenue", 0.4],
-            14:[0, "Magenta", "Virginia Avenue", 0.4],
-            15:[0, "Railroad", "Pennsylvania Railroad", 0.8],
-            16:[0, "Orange", "St James Place", 0.9],
+            11:[0, "Magenta", "St Charles Place"],
+            12:[0, "Utilities", "Electric Company"],
+            13:[0, "Magenta", "States Avenue"],
+            14:[0, "Magenta", "Virginia Avenue"],
+            15:[0, "Railroad", "Pennsylvania Railroad"],
+            16:[0, "Orange", "St James Place"],
             17:[-2, "Community Chest"],
-            18:[0, "Orange", "Tennessee Avenue", 0.9],
-            19:[0, "Orange", "New York Avenue", 0.9],
+            18:[0, "Orange", "Tennessee Avenue"],
+            19:[0, "Orange", "New York Avenue"],
             20:[-2, "Free Parking"],
-            21:[0, "Red", "Kentucky Avenue", 1],
+            21:[0, "Red", "Kentucky Avenue"],
             22:[-2, "Chance"],
-            23:[0, "Red", "Indiana Avenue", 1],
-            24:[0, "Red", "Illinois Avenue", 1],
-            25:[0, "Railroad", "B&O Railroad", 0.8],
-            26:[0, "Yellow", "Atlantic Avenue", 0.6],
-            27:[0, "Yellow", "Ventnor Avenue", 0.6],
-            28:[0, "Utilities", "Water Works", 0.1],
-            29:[0, "Yellow", "Marvin Gardens", 0.6],
+            23:[0, "Red", "Indiana Avenue"],
+            24:[0, "Red", "Illinois Avenue"],
+            25:[0, "Railroad", "B&O Railroad"],
+            26:[0, "Yellow", "Atlantic Avenue"],
+            27:[0, "Yellow", "Ventnor Avenue"],
+            28:[0, "Utilities", "Water Works"],
+            29:[0, "Yellow", "Marvin Gardens"],
             30:[-2, "Go To Jail"],
-            31:[0, "Green", "Pacific Avenue", 0.5],
-            32:[0, "Green", "North Carolina Avenue", 0.5],
+            31:[0, "Green", "Pacific Avenue"],
+            32:[0, "Green", "North Carolina Avenue"],
             33:[-2, "Community Chest"],
-            34:[0, "Green", "Pennsylvania Avenue", 0.5],
-            35:[0, "Railroad", "Shortline", 0.8],
+            34:[0, "Green", "Pennsylvania Avenue"],
+            35:[0, "Railroad", "Shortline"],
             36:[-2, "Chance"],
-            37:[0, "DarkBlue", "Park Place", 0.7],
+            37:[0, "DarkBlue", "Park Place"],
             38:[-2, "Luxury Tax"],
-            39:[0, "DarkBlue", "Boardwalk", 0.7],
+            39:[0, "DarkBlue", "Boardwalk"],
   }
 
   def __init__(self, player=False):
@@ -64,7 +64,6 @@ class board(rules.BoardSpaces, rules.TitleDeedCards):
     else: 
       self.color = self.Board[x][1] 
       self.name = self.Board[x][2] 
-      self.numInColor = self.Board[x][3]
       self.price = self.board[x][3]
 
   def updatePosition(self, sum):
@@ -83,15 +82,16 @@ class board(rules.BoardSpaces, rules.TitleDeedCards):
     
     if self.square == -2:
       if self.isPlayer == False:
+        #Say what square it lands on.
         print ("I landed on " + self.name)
       else:
         print("You've landed on " + self.name)
-      match self.name:
-        case "Community Chest":
+      """match self.name:
+        case "CommunityChest":
           self.pullCard()
         case "Chance":
           self.pullCard()
-        case "Income Tax":
+        case "IncomeTax":
           if self.isPlayer == False:
             self.giveMoney(min(200, self.robotMoneySum * 0.1))
           else:
@@ -100,13 +100,30 @@ class board(rules.BoardSpaces, rules.TitleDeedCards):
           self.currPos = 10
           self.inJail = True
         case "Luxury Tax":
-          self.giveMoney(75)
+          self.giveMoney(75)"""
+      
+      if self.name == "CommunityChest":
+        self.pullCard()
+      elif self.name == "Chance":
+        self.pullCard()
+      elif self.name == "IncomeTax":
+        if self.isPlayer == False:
+          self.giveMoney(min(200, self.robotMoneySum * 0.1))
+        else:
+          self.giveMoney(min(200, self.playerMoneySum * 0.1))
+      elif self.name == "Go To Jail":
+        self.currPos = 10
+        self.inJail = True
+      elif self.name == "Luxury Tax":
+        self.giveMoney(75)
 
     else:
       if self.isPlayer == False:
         print("I landed on " + self.name)
+        self.payRent()
       else:
         print("You've landed on " + self.name)
+        self.payRent()
 
   def updateOwnership(self, change):
     self.namingShortcut()
@@ -121,6 +138,8 @@ class board(rules.BoardSpaces, rules.TitleDeedCards):
       self.square = -1
     print("Board state updated.")
 
+
+
 class Assets(board):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -131,7 +150,7 @@ class Assets(board):
     self.playerMoneySum = 1500
     
     self.ownedProperties = {"Brown": {"Mediterranean Avenue": [False, 0, False], "Baltic Avenue": [False, 0, False]},
-                            "LightBlue":{"Oriental Avenue": [False, 0, False], "Vermont Avenue": [False, 0], "Connecticut Avenue": [False, 0, False]},
+                            "LightBlue":{"Oriental Avenue": [False, 0, False], "Vermont Avenue": [False, 0, False], "Connecticut Avenue": [False, 0, False]},
                             "Magenta":{"St Charles Place": [False, 0, False], "States Avenue": [False, 0, False], "Virginia Avenue":[False, 0, False]},
                             "Orange":{"St James Place": [False, 0, False], "Tennessee Avenue": [False, 0, False], "New York Avenue": [False, 0, False]},
                             "Red": {"Kentucky Avenue": [False, 0, False], "Indiana Avenue": [False, 0, False], "Illinois Avenue": [False, 0, False]},
@@ -139,11 +158,11 @@ class Assets(board):
                             "Green":{"Pacific Avenue": [False, 0, False], "North Carolina Avenue": [False, 0, False], "Pennsylvania Avenue": [False, 0, False]},
                             "DarkBlue":{"Park Place": [False, 0, False], "Boardwalk": [False, 0, False]}, 
                             "Railroad":{"Reading Railroad": [False, 0, False], "Pennsylvania Railroad":[False, 0, False], "B&O Railroad":[False, 0, False], "Shortline":[False, 0, False]},
-                            "Utilities":{"Electric Company:":[False, 0, False], "Water Works":[False, 0, False]}
+                            "Utilities":{"Electric Company:": [False, 0, False], "Water Works":[False, 0, False]},
     }
     
     self.playerOwnedProperties = {"Brown": {"Mediterranean Avenue": [False, 0, False], "Baltic Avenue": [False, 0, False]},
-                            "LightBlue":{"Oriental Avenue": [False, 0, False], "Vermont Avenue": [False, 0], "Connecticut Avenue": [False, 0, False]},
+                            "LightBlue":{"Oriental Avenue": [False, 0, False], "Vermont Avenue": [False, 0, False], "Connecticut Avenue": [False, 0, False]},
                             "Magenta":{"St Charles Place": [False, 0, False], "States Avenue": [False, 0, False], "Virginia Avenue":[False, 0, False]},
                             "Orange":{"St James Place": [False, 0, False], "Tennessee Avenue": [False, 0, False], "New York Avenue": [False, 0, False]},
                             "Red": {"Kentucky Avenue": [False, 0, False], "Indiana Avenue": [False, 0, False], "Illinois Avenue": [False, 0, False]},
@@ -151,7 +170,7 @@ class Assets(board):
                             "Green":{"Pacific Avenue": [False, 0, False], "North Carolina Avenue": [False, 0, False], "Pennsylvania Avenue": [False, 0, False]},
                             "DarkBlue":{"Park Place": [False, 0, False], "Boardwalk": [False, 0, False]}, 
                             "Railroad":{"Reading Railroad": [False, 0, False], "Pennsylvania Railroad":[False, 0, False], "B&O Railroad":[False, 0, False], "Shortline":[False, 0, False]},
-                            "Utilities":{"Electric Company:":[False, 0, False], "Water Works":[False, 0, False]}
+                            "Utilities":{"Electric Company:": [False, 0, False], "Water Works":[False, 0, False]},
     }
     self.robotOwnedProperty, self.playerOwnedProperty = [], []
     self.robotNumProperties, self.playerNumProperties = 0, 0
@@ -183,7 +202,8 @@ class Assets(board):
             amount -= (i * temp)
           if amount == 0:
             return
-
+  
+  #FIX IMMEDIATELY. Cash cannot go negative. 
   def giveMoney(self, amount):
     if self.isPlayer == False:
       self.robotMoneySum -= amount
@@ -205,7 +225,7 @@ class Assets(board):
             amount -= (i * temp)
           if amount == 0:
             return
-
+            
   def isASet(self, color):
     if self.isPlayer == False:
       for name in self.ownedProperties[color]:
@@ -228,6 +248,7 @@ class Assets(board):
         if self.ownedProperties[color][name][2] == True:
           return True
     return False
+
         
 
 
@@ -246,6 +267,7 @@ class Actions(Assets, rules.ChanceCommunityCards):
           self.ownedProperties[self.color][self.name][0] = True
           self.robotOwnedProperty.append(self.name)
           self.robotNumProperties += 1
+          #Social aspect here, saying that they bought xxx property.
           if self.isASet(self.color) and (self.color != "Utilities" or self.color != "Railroads"):
             self.ownedSets.add(self.color)
             
@@ -259,7 +281,7 @@ class Actions(Assets, rules.ChanceCommunityCards):
           self.playerNumProperties += 1
           if self.isASet(self.color) and (self.color != "Utilities" or self.color != "Railroads"):
             self.ownedSets.add(self.color)
-
+    
   def buyHouse(self, color):
     prop = dict()
     if self.isPlayer == False and self.isSet(color) and self.isMortgaged(color) == False:
@@ -269,6 +291,7 @@ class Actions(Assets, rules.ChanceCommunityCards):
       if all(value == most_houses for value in prop.values()):
         house = rd.choice([prop.keys()])
         self.addHouse(color, house)
+        #Social Here, bragging about buying the house
       else:
         least_houses = min(prop.values())
         min_keys = [m for m in prop if prop[m] == least_houses]
@@ -301,7 +324,7 @@ class Actions(Assets, rules.ChanceCommunityCards):
     
     if x >= 350 and self.isPlayer == False:
       print("Adding a house to " + choice)
-      self.giveMoney(self.getBuildCost(choice))
+      self.giveMoney(self.cards[choice][1])
       if self.isPlayer == True:
         self.playerOwnedProperties[color][choice][1] += 1
       else:
@@ -339,19 +362,38 @@ class Actions(Assets, rules.ChanceCommunityCards):
           choice2 = input("Are your sure? You will not be able to buy houses in this color set anymore [y/n]: ")
           match choice2:
             case "y":
-              self.gainMoney(self.mortgageProperty(choice))
+              self.gainMoney(self.cards[choice][2])
               self.playerOwnedProperties[color][choice][2] = True
               return
             case "n":
               return
         elif choice in prop[color]:
-          self.gainMoney(self.mortgageProperty(choice))
+          self.gainMoney(self.cards[choice][2])
           self.playerOwnedProperties[color][choice][2] = True
     else:
       return
           
   def readCardCC(self, cardNum):
-    match cardNum:
+    #social here, talking about the card pulled.
+    if cardNum == 1:
+      if self.isPlayer == False:
+        self.giveMoney(self.house * 40 + self.hotel * 110)
+      else:
+        self.giveMoney(self.playerHouse * 40 + self.playerHotel * 110)
+    elif cardNum == 2:
+      self.currPos = 0
+      self.passGo()
+
+    elif cardNum == 3:
+      self.gainMoney(10)
+    elif cardNum == 4:
+      self.gainMoney(50)
+    elif cardNum == 5:
+      self.gainMoney(100)
+    elif cardNum == 6:
+      self.gainMoney(10 * self.numPlayers)
+      
+    """match cardNum:
       case 1:
         if self.isPlayer == False:
           self.giveMoney(self.house * 40 + self.hotel * 110)
@@ -369,7 +411,8 @@ class Actions(Assets, rules.ChanceCommunityCards):
       case 6:
         self.gainMoney(10 * self.numPlayers)
       case 6:
-        return False
+        return False"""
+    
 
   def pullCard(self):
     self.card = self.listOfCC.pop(0)
@@ -382,17 +425,46 @@ class Actions(Assets, rules.ChanceCommunityCards):
     elif self.currPos in [7, 22, 36]:
       return
 
+  def payRent(self):
+    self.namingShortcut()
+    if self.isPlayer == False:
+      if self.playerOwnedProperties[self.color][self.name][2] == True:
+        print("This property is mortgaged.")
+      elif self.square == -1 and self.color in self.playerOwnedSets:
+        house = self.playerOwnedProperties[self.color][self.name][1]
+        if house > 4:
+          self.giveMoney(self.rentLookup(self.name, isSet = True, numHotel=True))
+        else:
+          self.giveMoney(self.rentLookup(self.name, isSet = True, numHouses=house))
+      elif self.square == -1:
+        self.giveMoney(self.rentLookup(self.name))
+      #social here, anger.
+        
+    else:
+    	if self.ownedProperties[self.color][self.name][2] == True:
+    		print("This property is mortgaged.")
+    	elif self.square == 1 and self. color in self.ownedSets:
+    		house = self.ownedProperties[self.color][self.name][1]
+    		if house > 4:
+    			self.giveMoney(self.rentLookup(self.name, isSet = True, numHotel = True))
+    		else:
+    			self.giveMoney(self.rentLookup(self.name, isSet == True, numHouses=house))
+    	elif self.square == 1:
+    		self.giveMoney(self.rentLookup(self.name))
+
   def endTurn(self):
     self.turnCounter -= 1
     if self.isPlayer == True:
       self.isPlayer = False
     else:
       self.isPlayer = True
+    #Taunting here. Social
 
   def checkBankruptcy(self):
     if self.isPlayer == True:
       if self.playerMoneySum == 0 and self.playerNumProperties == 0:
         return True
+        #Trump voice, demand recount.
     else:
       if self.robotMoneySum == 0 and self.robotNumProperties == 0:
         return True
@@ -404,6 +476,7 @@ class Jail(Actions):
     self.inJail = False
 
   def goToJail(self):
+    #Trump voice
     if self.currPos == 30:
       self.inJail = True
     elif self.currPos in [2, 7, 17, 22, 33, 36]:
@@ -411,11 +484,13 @@ class Jail(Actions):
         self.inJail = True
 
   def payFine(self):
+    #Trump voice
     self.giveMoney(50)
 
   def rollOut(self):
     temp = rules.rollDice()
     if temp[0] == temp[1]:
+      #Bragging
       return True
     return False
 
@@ -426,17 +501,48 @@ class Decisions(Jail):
     endTurn = False
     while endTurn == False:
       if c == 1:
-        choice = input("(1) Buy\n(2) Sell Houses\n(3)Mortgage Properties\n(4) Check Balance\n(5) Declare Bankruptcy\n(6) End Turn: ")
+        choice = int(input("(1) Buy\n(2) Sell Houses\n(3)Mortgage Properties\n(4) Check Balance\n(5) Declare Bankruptcy\n(6) End Turn: "))
       else:
-        choice = input("(1) Buy\n(2) Sell\n(3)Mortgage\n(4) Check Balance\n(5) Declare Bankruptcy\n(6) Skip Turn\n(7) Roll Dice: ")
-      match int(choice):
+        choice = int(input("(1) Buy\n(2) Sell\n(3)Mortgage\n(4) Check Balance\n(5) Declare Bankruptcy\n(6) Skip Turn\n(7) Roll Dice: "))
+      if choice == 1:
+        choice2 = int(input("(1) Buy Property\n(2) Buy Upgrade: "))
+        if choice2 == 1:
+          self.buy()
+        elif choice2 == 2:
+          if len(self.playerOwnedSets) != 0:
+            print("Your current completed sets are " + str(self.playerOwnedSets))
+            i = input("What color set would you like to build on?: ")
+            self.buyHouse(i)
+          else:
+            print("You don't have any completed sets!")
+      elif choice == 3:
+        self.mortgage()
+      elif choice == 4:
+        print(self.playerMoneySum)
+        print("A more detailed breakdown: " + str(self.playerMoney))
+        if len(self.playerOwnedProperty) != 0:
+          print(self.playerOwnedProperty)
+        else:
+          print("You own no properties!")
+
+      elif choice == 5:
+        self.checkBankruptcy()
+
+      elif choice == 6:
+        break
+      elif choice == 7:
+        r = rules.rollDice()
+        self.updatePosition(r[0] + r[1])
+        c = 1
+    return
+    """match int(choice):
         case 1:
           choice2 = input("(1) Buy Properties\n(2) Improve Property: ")
           match int(choice2):
             case 1:
               self.buy()
             case 2:
-              if len(self.playerOwnedSets) > 0:
+              if len(self.playerOwnedSets) != 0:
                 print("Your current completed sets are " + str(self.playerOwnedSets))
                 i = input("What color set would you like to build on?: ")
                 self.buyHouse(i)
@@ -466,29 +572,39 @@ class Decisions(Jail):
         case 7:
           r = rules.rollDice()
           self.updatePosition(r[0] + r[1])
-          c = 1
+          c = 1"""
           
   
   def playerStartTurn(self):
     print("It is your turn! Would you like to roll the dice?")
     playerIn = input("[y/n]: ")
-    match playerIn:
+    if playerIn == "y":
+      r = rules.rollDice()
+      self.updatePosition(r[0] + r[1])
+      return 1
+    return 0
+  
+    """match playerIn:
       case "y":
         r = rules.rollDice()
         self.updatePosition(r[0] + r[1])
         return 1
       case "n":
-        return 0
+        return 0"""
 
   def robotDecision(self):
     ownedWeights = {}
+    self.namingShortcut()
+    if self.square == 0:
+      self.buy()
+    elif self.square == -1:
+      self.payRent()
     if len(self.ownedSets) > 0:
       for color in self.ownedSets:
         ownedWeights[color] = self.weights[color]
       max_key = max(ownedWeights, key=ownedWeights.get)
       self.buyHouse(max_key)
 
-    self.buy()
     
         
 
@@ -513,11 +629,12 @@ class PlayGame(Decisions):
     self.d.updatePosition(r[0] + r[1])
     self.d.robotDecision()
     self.d.endTurn()
-    print(self.d.turnCounter)
+    print("\n\n")
     
   def playerTurn(self):
     self.d.playerDecisions()
     self.d.endTurn()
+    print("\n\n")
     
   def playGame(self):
     self.startGame()
@@ -556,3 +673,4 @@ def playTurn():
 
 if __name__ == "__main__":
   playTurn()
+
