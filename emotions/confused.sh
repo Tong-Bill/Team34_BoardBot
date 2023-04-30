546ccba11e058269a3599320dcdd0842a0a989af
Bill Tong <66698504+Tong-Bill@users.noreply.github.com>
2023-04-29 18:43:20 -0700

    Update and rename confused.sh to emotions/confused.sh

---
+++ emotions/confused.sh
@@ -0,0 +1,7 @@
+# Author: Bill Tong
+# Purpose: executes the xdisplay_image script, changing baxter display to the confused face
+
+source /home/team34/ros_ws/devel/setup.bash
+
+rosrun baxter_tools xdisplay_image.py --file=`rospack find baxter_tools`/images/ConfusedSWGray.jpg;
+
