37f52f4d81a3f77b7c5c1a869627c46dee8966b6
Bill Tong <66698504+Tong-Bill@users.noreply.github.com>
2023-04-29 18:48:01 -0700

    Update and rename sassy.sh to emotions/sassy.sh

--- sassy.sh
+++
@@ -1,7 +0,0 @@
-# Author: Bill Tong
-# Purpose: executes the xdisplay_image script, changing baxter display to the sassy face
-
-source /home/bill/ros_ws/devel/setup.bash
-
-rosrun baxter_sim_examples xdisplay_image.py --file=`rospack find baxter_sim_examples`/images/SassyNWPurple.jpg;
-
