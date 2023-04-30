1214009ecb16c5bc529f924c1239d819f6ac914c
Bill Tong <66698504+Tong-Bill@users.noreply.github.com>
2023-04-29 18:50:16 -0700

    Create worried.sh

---
+++ emotions/worried.sh
@@ -0,0 +1,6 @@
+# Author: Bill Tong
+# Purpose: executes the xdisplay_image script, changing baxter display to the worried face
+
+source /home/team34/ros_ws/devel/setup.bash
+
+rosrun baxter_tools xdisplay_image.py --file=`rospack find baxter_tools`/images/WorriedNWGreen.jpg;
