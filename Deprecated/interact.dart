071ed7814c65bdcf312f5a0ebe7e2b3421e28486
Bill Tong <66698504+Tong-Bill@users.noreply.github.com>
2023-05-09 22:52:29 -0700

    Update interact.dart

--- Deprecated/interact.dart
+++ Deprecated/interact.dart
@@ -1,3 +1,6 @@
+// Author: Bill Tong
+// Scrapped in favor of a new Web User Interface
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
